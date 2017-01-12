2fadd0a4a83caa8d6675aeb797206fd935795591
William Archinal <william.archinal@gmail.com>
2017-01-12 16:42:15 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Constraint Solver #
+This is a generic solver of constraint satisfaction problems written in JavaScript.
+
+### Example Usage ###
+Usage...
---
+++ main.js
@@ -0,0 +1,7 @@
+var ConstraintSatisfactionEngine = {};
+
+ConstraintSatisfactionEngine.solveProblem = function (state, getLegalActions, applyAction, config) {
+
+};
+
+module.exports = ConstraintSatisfactionEngine;
